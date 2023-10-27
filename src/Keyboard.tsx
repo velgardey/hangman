e6e718eb794f95a398c93b4bde03b1804af27e72
Mriganka Dey <deymriganka2004@gmail.com>
2023-10-27 14:25:56 +0530

    merged with an old commit to fix some major issues

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,71 +1,64 @@
-import styles from './keyboard.module.css';
+import styles from "./Keyboard.module.css"
 
 const KEYS = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
 ]
 
 type KeyboardProps = {
-  disabled?: boolean
-  activeLetters: string[]
-  inactiveLetters: string[]
-  addGuessedLetter: (letter: string) => void
+    disabled?: boolean
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter: (letter: string) => void
 }
 
-export function Keyboard({
-  activeLetters,
-  inactiveLetters,
-  addGuessedLetter,
-  disabled = false,
-}: KeyboardProps) {
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
-        gap: ".5rem",
-      }}
-    >
-      {KEYS.map(key => {
-        const isActive = activeLetters.includes(key)
-        const isInactive = inactiveLetters.includes(key)
-        return (
-          <button
-            onClick={() => addGuessedLetter(key)}
-            className={`${styles.btn} ${isActive ? styles.active : ""} ${
-              isInactive ? styles.inactive : ""
-            }`}
-            disabled={isInactive || isActive || disabled}
-            key={key}
-          >
-            {key}
-          </button>
-        )
-      })}
-    </div>
-  )
+export function Keyboard({activeLetters, inactiveLetters, addGuessedLetter, disabled = false,
+                         }: KeyboardProps) {
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+                gap: ".5rem",
+            }}>
+            {KEYS.map(key => {
+                const isActive = activeLetters.includes(key)
+                const isInactive = inactiveLetters.includes(key)
+                return (
+                    <button
+                        onClick={() => addGuessedLetter(key)}
+                        className={`${styles.btn} ${isActive ? styles.active : ""} ${
+                            isInactive ? styles.inactive : ""
+                        }`}
+                        disabled={isInactive || isActive || disabled}
+                        key={key}>
+                        {key}
+                    </button>
+                )})}
+        </div>
+    )
 }
